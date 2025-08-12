9c91285631828b92a7ef45cb64302f1ff5807a34
DylPorter <thiendylanporter@gmail.com>
2025-08-13 01:59:31 +0800

    added experience and education functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { LuSun, LuMoon, LuCopy, LuSend, LuCopyCheck } from "react-icons/lu";
+import { LuSun, LuMoon, LuCopy, LuSend, LuCopyCheck, LuExternalLink } from "react-icons/lu";
 import { FaLinkedinIn, FaGithub, FaRegEnvelope } from "react-icons/fa6";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
   });
   const [submitting, setSubmitting] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [active, setActive] = useState("experience");
 
   const email = "info@tdporter.dev"
 
@@ -111,9 +112,9 @@ function App() {
 
   return (
     <div id="top" className={`${theme === "dark" ? "dark" : ""} bg-neutral-100 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100 min-h-screen w-full transition-colors duration-500`}>
-      <header className={`sticky top-0 z-50 mx-auto p-4 md:p-8 transition-all ${scrolled ? "duration-1200 max-w-[370px] md:max-w-lg" : "duration-1200 max-w-full md:max-w-3xl"}`}> 
+      <header className={`sticky top-0 z-50 mx-auto px-4 py-8 md:px-8 transition-all ${scrolled ? "duration-1200 max-w-[410px] md:max-w-lg" : "duration-1200 max-w-full md:max-w-3xl"}`}> 
         <nav className="card flex pl-8 p-4 items-center justify-between">
-          <ul className="flex gap-8">
+          <ul className="flex gap-6 md:gap-8">
             <li><a 
               href="/" 
               className="link"
@@ -122,6 +123,14 @@ function App() {
                 scrollToSection("top");
               }}
             >Home</a></li>
+            <li><a 
+              href="#skills" 
+              className="link"
+              onClick={(e) => {
+                e.preventDefault();
+                scrollToSection("skills");
+              }}
+            >Skills</a></li>
             <li><a 
               href="#projects" 
               className="link"
@@ -145,65 +154,170 @@ function App() {
         </nav>
       </header>
 
-      <main id="home" className="max-w-3xl px-4 md:px-8 mx-auto flex flex-col gap-4">
+      <main className="max-w-3xl px-4 md:px-8 mx-auto flex flex-col gap-4">
 
-        <section className="card p-10 flex flex-col md:flex-row justify-between gap-8 md:gap-12 items-center">
-          <div className="w-full md:w-2/5 flex-shrink-0 h-[275px] md:h-[300px] overflow-hidden rounded-xl">
-            <img
-              className="w-full h-full object-cover md:scale-140 object-[50%_25%] md:translate-y-8 md:-translate-x-2"
-              alt="Photo of me" 
-              src="./IMG-20250131-WA0057.jpg"
-            />
-          </div>
-          
-          <div className="flex flex-col items-center md:items-stretch">
-            <h1 className="text-5xl font-bold">Hi, I'm Dylan.</h1>
-            <div className="mt-5 flex flex-row flex-wrap gap-x-6 md:flex-col items-center justify-center md:items-start">
-              <p>Developer
-                <a href="https://collectiveglobal.net" target="_blank" className="link">{" "}@ Collective Global</a>
-              </p>
-              <p>Instructor
-                <a href="https://bsd.education" target="_blank" className="link">{" "}@ BSD Education</a>
-              </p>
-              <p>Student
-                <a href="https://hku.hk" target="_blank" className="link">{" "}@ The University of Hong Kong</a>
-              </p>
+        <section id="home" className="flex flex-col gap-4">
+          <div className="card p-10 items-center flex flex-col md:flex-row gap-8 md:gap-12 justify-between">
+            <div className="w-full md:w-2/5 flex-shrink-0 h-[275px] md:h-[300px] overflow-hidden rounded-xl">
+              <img
+                className="w-full h-full object-cover md:scale-140 object-[50%_25%] md:translate-y-8 md:-translate-x-2"
+                alt="Photo of me" 
+                src="./IMG-20250131-WA0057.jpg"
+              />
             </div>
-            <p className="mt-4 text-balance text-center md:text-left">I develop solutions for startup companies, teach students throughout Hong Kong, and study computer science in university.</p>
-            <div className="flex flex-wrap items-center gap-2 mt-5">
-              <a href="./resume.pdf" target="_blank" className="px-4 py-2 text-sm button">Résumé</a>
-              <a href="https://linkedin.com/in/tdporter" target="_blank" className="p-3 hover-button"><FaLinkedinIn /></a>
-              <a href="https://github.com/dylporter" target="_blank" className="p-3 hover-button"><FaGithub/></a>
+            
+            <div className="flex flex-col items-center md:items-stretch">
+              <h1 className="text-5xl font-bold">Hi, I'm Dylan.</h1>
+              <div className="mt-5 flex flex-row flex-wrap gap-x-6 md:flex-col items-center justify-center md:items-start">
+                <p>Developer
+                  <a href="https://collectiveglobal.net" target="_blank" className="link">{" "}@ Collective Global</a>
+                </p>
+                <p>Instructor
+                  <a href="https://bsd.education" target="_blank" className="link">{" "}@ BSD Education</a>
+                </p>
+                <p>Student
+                  <a href="https://hku.hk" target="_blank" className="link">{" "}@ The University of Hong Kong</a>
+                </p>
+              </div>
+              <p className="mt-4 text-balance text-center md:text-left">I develop solutions for startup companies, teach students throughout Hong Kong, and study computer science in university.</p>
+              <div className="flex flex-wrap items-center gap-2 mt-5">
+                <a href="./resume.pdf" target="_blank" className="px-4 py-2 text-sm button">Résumé</a>
+                <a href="https://linkedin.com/in/tdporter" target="_blank" className="p-3 hover-button"><FaLinkedinIn /></a>
+                <a href="https://github.com/dylporter" target="_blank" className="p-3 hover-button"><FaGithub/></a>
 
-              <div className="relative inline-block text-center">
-                <button id="email-dropdown-button" onClick={toggleEmailDropdown} className="p-3 hover-button">
-                  <FaRegEnvelope />
-                </button>
+                <div className="relative inline-block text-center">
+                  <button id="email-dropdown-button" onClick={toggleEmailDropdown} className="p-3 hover-button hover:cursor-pointer">
+                    <FaRegEnvelope />
+                  </button>
 
-                {emailDropdownOpen && (
-                  <div
-                    id="email-dropdown-menu"
-                    className={`card p-3 absolute bottom-full mb-2 left-1/2 -translate-x-1/2 w-50 z-20
-                      ${dropdownAnimating ? "dropdown-enter" : "dropdown-leave"}
-                    `}
-                  >
-                    <div className="p-3 text-sm break-words text-neutral-500 dark:text-neutral-400">{email}</div>
-                    <div className="flex gap-1 text-sm items-center justify-center">
-                      <button
-                        onClick={copyEmail}
-                        className="w-full py-2 hover-button flex justify-center items-center gap-2"
-                      >
-                        {copied ? <LuCopyCheck className="text-neutral-700 dark:text-neutral-300"/> : <LuCopy />}
-                        {copied ? <span className="text-neutral-700 dark:text-neutral-300">Copied!</span> : "Copy"}
-                      </button>
-                      <a href={`mailto:${email}`} className="w-full py-2 hover-button gap-2">
-                        <LuSend />Send
-                      </a>
+                  {emailDropdownOpen && (
+                    <div
+                      id="email-dropdown-menu"
+                      className={`card p-3 absolute bottom-full mb-2 left-1/2 -translate-x-1/2 w-50 z-20
+                        ${dropdownAnimating ? "dropdown-enter" : "dropdown-leave"}
+                      `}
+                    >
+                      <div className="p-3 text-sm break-words text-neutral-500 dark:text-neutral-400">{email}</div>
+                      <div className="flex gap-1 text-sm items-center justify-center">
+                        <button
+                          onClick={copyEmail}
+                          className="w-full py-2 hover-button flex justify-center items-center gap-2 hover:cursor-pointer"
+                        >
+                          {copied ? <LuCopyCheck className="text-neutral-700 dark:text-neutral-300"/> : <LuCopy />}
+                          {copied ? <span className="text-neutral-700 dark:text-neutral-300">Copied!</span> : "Copy"}
+                        </button>
+                        <a href={`mailto:${email}`} className="w-full py-2 hover-button gap-2">
+                          <LuSend />Send
+                        </a>
+                      </div>
                     </div>
-                  </div>
-                )}
+                  )}
+                </div>
               </div>
             </div>
+          </div>
+
+          <div className="card p-2 mt-4">
+            <div className="flex gap-2 w-full h-full rounded-lg">
+              <button
+                className={`w-1/2 py-1.5 text-sm rounded-lg hover:cursor-pointer transition-colors duration-500 ${active === "experience" ? "bg-neutral-200 dark:bg-neutral-800" : "text-neutral-500 dark:text-neutral-400"}`}
+                onClick={() => setActive("experience")}
+              >Experience</button>
+              <button 
+                className={`w-1/2 py-1.5 text-sm rounded-lg hover:cursor-pointer transition-colors duration-500 ${active === "education" ? "bg-neutral-200 dark:bg-neutral-800" : "text-neutral-500 dark:text-neutral-400"}`}
+                onClick={() => setActive("education")}
+              >Education</button>
+            </div>
+          </div>
+
+          <div className="relative">
+            {/* Experience Card */}
+            <div className={`card p-8 text-sm flex flex-col gap-4 ${active === "experience" ? "block" : "hidden"}`}>
+
+              {/* Collective */}
+              <div>
+                <div className="flex items-center mb-2">
+                  <img className="icon mr-4" src="./collective_logo.jpg"></img>
+                  <div className="flex flex-col gap-0.25">
+                    <p className="font-bold text-base">Collective Global</p>
+                    <p className="-mt-0.5">Full Stack Developer (Contract)</p>
+                    <p className="text-xs dark:text-neutral-400">Jul 2024 – Present</p>
+                  </div>
+                </div>
+                <p className="ml-16">blablabla this is some of the cool stuff that i did hopefully that's useful for you</p>
+              </div>
+
+              <hr className="border-t border-neutral-300 dark:border-neutral-700 my-1" />
+
+              {/* BSD Teaching */}
+              <div>
+                <div className="flex items-center mb-2">
+                  <img className="icon mr-4" src="./bsdeducation_logo.jpg"></img>
+                  <div className="flex flex-col gap-0.25">
+                    <p className="font-bold text-base">BSD Education</p>
+                    <p className="-mt-0.5">Coding Instructor (Part-time)</p>
+                    <p className="text-xs dark:text-neutral-400">May 2025 – Present</p>
+                  </div>
+                </div>
+                <p className="ml-16">I did teaching here it was very cool</p>
+              </div>
+
+              <hr className="border-t border-neutral-300 dark:border-neutral-700 my-1" />
+
+              {/* BSD Intern */}
+              <div>
+                <div className="flex items-center mb-2">
+                  <img className="icon mr-4" src="./bsdeducation_logo.jpg"></img>
+                  <div className="flex flex-col gap-0.25">
+                    <p className="font-bold text-base">BSD Education</p>
+                    <p className="-mt-0.5">Developer (Internship)</p>
+                    <p className="text-xs dark:text-neutral-400">Sep 2023 – Mar 2024</p>
+                  </div>
+                </div>
+                <p className="ml-16">blablabla this is some of the cool stuff that i did hopefully that's useful for you</p>
+              </div>
+
+            </div>
+
+            {/* Education Card */}
+            <div className={`card p-8 text-sm flex flex-col gap-4 ${active === "education" ? "block" : "hidden"}`}>
+
+              {/* HKU */}
+              <div>
+                <div className="flex items-center mb-2">
+                  <img className="icon mr-4" src="./hku_logo.jpeg"></img>
+                  <div className="flex flex-col gap-0.25">
+                    <p className="font-bold text-base">The University of Hong Kong</p>
+                    <p className="-mt-0.5">BEng in Computer Science</p>
+                    <p className="text-xs dark:text-neutral-400">Sep 2024 – Due Jun 2028</p>
+                  </div>
+                </div>
+                <p className="ml-16">blablabla this is some of the cool stuff that i did hopefully that's useful for you</p>
+              </div>
+
+              <hr className="border-t border-neutral-300 dark:border-neutral-700 my-1" />
+
+              {/* DSC */}
+              <div>
+                <div className="flex items-center mb-2">
+                  <img className="icon mr-4" src="./dsc_logo.jpg"></img>
+                  <div className="flex flex-col gap-0.25">
+                    <p className="font-bold text-base">DSC International School</p>
+                    <p className="-mt-0.5">Ontario Secondary School Diploma</p>
+                    <p className="text-xs dark:text-neutral-400">Jan 2017 – Jun 2024</p>
+                  </div>
+                </div>
+                <p className="ml-16">blablabla this is some of the cool stuff that i did hopefully that's useful for you</p>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+        <section id="skills" className="pt-32 -mt-24 flex flex-col gap-8">
+          <h2 className="text-3xl font-bold text-center md:text-start">Skills</h2>
+          <div className="card p-8">
+              
           </div>
         </section>
         
@@ -232,7 +346,7 @@ function App() {
                 <div className="pill">React</div>
                 <div className="pill">PostgreSQL</div>
               </div>
-              <a href="https://app.sourcinggpt.ai" target="_blank" className="self-start px-4 py-2 text-sm button mt-5">View</a>
+              <a href="https://app.sourcinggpt.ai" target="_blank" className="self-start px-4 py-2 text-sm button mt-5 gap-2"><LuExternalLink/>View</a>
             </div>
 
             {/* TwinToys */}
@@ -246,7 +360,7 @@ function App() {
                 <div className="pill">Meshy API</div>
                 <div className="pill">WordPress</div>
               </div>
-              <a href="https://twintoys.co" target="_blank" className="self-start px-4 py-2 text-sm button mt-5">View</a>
+              <a href="https://twintoys.co" target="_blank" className="self-start px-4 py-2 text-sm button mt-5 gap-2"><LuExternalLink/>View</a>
             </div>
 
             {/* ARFixit */}
@@ -260,7 +374,7 @@ function App() {
                 <div className="pill">Vuforia MTG</div>
                 <div className="pill">3D Modelling</div>
               </div>
-              <a href="https://arfixit.co" target="_blank" className="self-start px-4 py-2 text-sm button mt-5">View</a>
+              <a href="https://arfixit.co" target="_blank" className="self-start px-4 py-2 text-sm button mt-5 gap-2"><LuExternalLink/>View</a>
             </div>
 
             {/* VETsage */}
@@ -284,7 +398,7 @@ function App() {
                 <div className="pill">React</div>
                 <div className="pill">PostgreSQL</div>
               </div>
-              <a href="https://app.vetsage.ai" target="_blank" className="self-start px-4 py-2 text-sm button mt-5">View</a>
+              <a href="https://app.vetsage.ai" target="_blank" className="self-start px-4 py-2 text-sm button mt-5 gap-2"><LuExternalLink/>View</a>
             </div>
           </div>
         </section>
@@ -369,7 +483,7 @@ function App() {
               scrollToSection("top");
             }}
           >{" "}tdporter.dev</a></p>
-          <p>Last updated: August 11th, 2025</p>
+          <p>Last updated: August 13th, 2025</p>
         </div>
       </footer>
     </div>
